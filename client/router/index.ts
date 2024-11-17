d83019fc5b9f4d580f3dc9fe3ffe2da3a831182d
manasa <manasa@manasas-MacBook-Air.local>
2024-11-17 18:22:45 -0500

    a5

--- client/router/index.ts
+++ client/router/index.ts
@@ -4,10 +4,18 @@ import { storeToRefs } from "pinia";
 import { createRouter, createWebHistory } from "vue-router";
 
 import { useUserStore } from "@/stores/user";
-import HomeView from "../views/HomeView.vue";
-import LoginView from "../views/LoginView.vue";
-import NotFoundView from "../views/NotFoundView.vue";
-import ProfileView from "../views/ProfileView.vue";
+
+// Importing all views
+import AlertingView from "@/views/AlertingView.vue";
+import HomeView from "@/views/HomeView.vue";
+import LoginView from "@/views/LoginView.vue";
+import MessagingView from "@/views/MessagingView.vue";
+import MonitoringView from "@/views/MonitoringView.vue";
+import NotFoundView from "@/views/NotFoundView.vue";
+import PostingView from "@/views/PostingView.vue";
+import ProfileView from "@/views/ProfileView.vue";
+import SettingView from "@/views/SettingView.vue";
+import TrustedContactsView from "@/views/TrustedContactsView.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -24,6 +32,42 @@ const router = createRouter({
       meta: { requiresAuth: true },
     },
     {
+      path: "/settings",
+      name: "Settings",
+      component: SettingView,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: "/alerting",
+      name: "Alerting",
+      component: AlertingView,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: "/trusted-contacts",
+      name: "TrustedContacts",
+      component: TrustedContactsView,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: "/messaging",
+      name: "Messaging",
+      component: MessagingView,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: "/posting",
+      name: "Posting",
+      component: PostingView,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: "/monitoring",
+      name: "Monitoring",
+      component: MonitoringView,
+      meta: { requiresAuth: true },
+    },
+    {
       path: "/login",
       name: "Login",
       component: LoginView,
@@ -31,20 +75,20 @@ const router = createRouter({
       beforeEnter: (to, from) => {
         const { isLoggedIn } = storeToRefs(useUserStore());
         if (isLoggedIn.value) {
-          return { name: "Settings" };
+          return { name: "Home" };
         }
       },
     },
     {
       path: "/:catchAll(.*)",
-      name: "not-found",
+      name: "NotFound",
       component: NotFoundView,
     },
   ],
 });
 
 /**
- * Navigation guards to prevent user from accessing wrong pages.
+ * Navigation guards to prevent user from accessing unauthorized pages.
  */
 router.beforeEach((to, from) => {
   const { isLoggedIn } = storeToRefs(useUserStore());
